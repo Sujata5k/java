2f281897734cb8746b3430bf18c0b7b9511c4d89
Sujata Pawar <139162044+Sujata5k@users.noreply.github.com>
2024-07-24 11:51:45 +0530

    Add files via upload

---
+++ calculation.java
@@ -0,0 +1,20 @@
+class calculation 
+{
+  public static void main(String a[])
+{
+
+int num1 = 12;
+int num2 = 34;
+
+int add = num1 + num2;
+int sub = num1 - num2;
+int div = num1/num2;
+int mul = num1 * num2;
+
+System.out.println("addition is  "+ add);
+System.out.println("Subtrction is :"+ sub);
+System.out.println("division is : "+ div);
+System.out.println("multiplication is : "+ mul);
+
+}
+}
